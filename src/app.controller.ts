2b8e928f4ab352cf6dd26f7b67b93463ba1f1c0a
nnavnita <navnitanandakumar@gmail.com>
2023-01-07 20:49:59 +1100

    change getBays to post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get('getBays')
+  @Post('getBays')
   async getBays(@Req() filter: { body: FilterObj }): Promise<any> {
     // TODO: get these values from the user
     const result = await this.appService.getBays(filter.body);
